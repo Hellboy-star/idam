57609b6016aea43fd66396758e3118c3317343c5
Hellboy-star <71907697+Hellboy-star@users.noreply.github.com>
2022-05-04 20:18:15 +0100

    Update databaseClient.dart

--- idam/lib/model/databaseClient.dart
+++ idam/lib/model/databaseClient.dart
@@ -1,9 +1,10 @@
 import 'dart:async';
 import 'dart:io';
-import 'package:idam/model/item.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+
+import 'item.dart';
 
 class DatabaseClient {
 
